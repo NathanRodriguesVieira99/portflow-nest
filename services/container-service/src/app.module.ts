29a52afcd8b569dfe26c9cb4447ab3cadcffd2a8
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-17 21:15:05 -0300

    feat(http): make custom axios http adapter NestJs module

--- services/container-service/src/app.module.ts
+++ services/container-service/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CacheModule } from './infrastructure/cache/cache.module';
 import { PrismaModule } from './infrastructure/database/prisma/prisma.module';
 import { LogsModule } from './infrastructure/logs/logs.module';
+import { HttpModule } from './infrastructure/http/http.module';
 
 @Module({
   providers: [],
   exports: [],
-  imports: [PrismaModule, CacheModule, LogsModule],
+  imports: [CacheModule, PrismaModule, LogsModule, HttpModule],
 })
 export class AppModule {}
