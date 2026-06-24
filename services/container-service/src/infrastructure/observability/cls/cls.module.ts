f2918979779d965987b94789e4a3ee5b232e8a61
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-23 23:24:30 -0300

    feat(cls): retrieve correlation id via header coming from kong

---
+++ services/container-service/src/infrastructure/observability/cls/cls.module.ts
@@ -0,0 +1,26 @@
+import { randomUUID } from 'node:crypto';
+import { Request } from 'express';
+import { Module } from '@nestjs/common';
+
+import { ClsModule as CLS } from 'nestjs-cls';
+
+/*  
+Kong envia o Header "X-Correlation-ID"
+CLS salva o correlation id
+Se não existir cria um novo correlation id
+ */
+@Module({
+  imports: [
+    CLS.forRoot({
+      global: true,
+      middleware: {
+        mount: true,
+        idGenerator: (req: Request) => {
+          const header = req.headers['x-correlation-id'];
+          return typeof header === 'string' ? header : randomUUID();
+        },
+      },
+    }),
+  ],
+})
+export class ClsModule {}
