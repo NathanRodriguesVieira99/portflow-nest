6e269621c928b7974d3273d3997c2113c14dd2d6
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-23 23:29:00 -0300

    refactor(redis): replace hardcoded process.env by env schema

--- services/container-service/src/infrastructure/cache/cache.module.ts
+++ services/container-service/src/infrastructure/cache/cache.module.ts
@@ -1,17 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { CacheModule as NestJsCacheModule } from '@nestjs/cache-manager';
 import KeyvRedis from '@keyv/redis';
+import { env } from '@/config/env';
 
 @Global()
 @Module({
   imports: [
     NestJsCacheModule.registerAsync({
       useFactory: () => ({
-        stores: [
-          new KeyvRedis(
-            `redis://${process.env.REDIS_HOST ?? 'localhost'}:${process.env.REFIS_PORT ?? 6379}`,
-          ),
-        ],
+        stores: [new KeyvRedis(`redis://${env.REDIS_HOST}:${env.REDIS_PORT}`)],
       }),
     }),
   ],
