efe3badcf344a69afb590b13fe55785964badbdf
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-24 17:58:07 -0300

    style(kafka): format imports

--- services/container-service/src/infrastructure/messaging/kafka/kafka.config.ts
+++ services/container-service/src/infrastructure/messaging/kafka/kafka.config.ts
@@ -1,9 +1,9 @@
 import { Transport, type KafkaOptions } from '@nestjs/microservices';
 
 import { KAFKA_CLIENTS } from './constants/clients';
+import { KAFKA_CONSUMER_GROUPS } from './constants/groups';
 
 import { env } from '@/config/env';
-import { KAFKA_CONSUMER_GROUPS } from './constants/groups';
 
 export const kafkaConfig: KafkaOptions = {
   transport: Transport.KAFKA,
