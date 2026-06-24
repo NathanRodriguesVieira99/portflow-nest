24b23e92f53d696cc0eb91de2c42e3ed6e6d1254
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-23 23:27:06 -0300

    feat: import infra modules

--- services/container-service/src/app.module.ts
+++ services/container-service/src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+
 import { CacheModule } from './infrastructure/cache/cache.module';
 import { PrismaModule } from './infrastructure/database/prisma/prisma.module';
-import { LogsModule } from './infrastructure/logs/logs.module';
+import { LogsModule } from './infrastructure/observability/logs/logs.module';
 import { HttpModule } from './infrastructure/http/http.module';
+import { KafkaModule } from './infrastructure/messaging/kafka/kafka.module';
+import { PrometheusModule } from './infrastructure/observability/prometheus/prometheus.module';
+import { ClsModule } from './infrastructure/observability/cls/cls.module';
 
 @Module({
-  providers: [],
-  exports: [],
-  imports: [CacheModule, PrismaModule, LogsModule, HttpModule],
+  imports: [
+    ClsModule,
+    CacheModule,
+    PrismaModule,
+    LogsModule,
+    HttpModule,
+    KafkaModule,
+    PrometheusModule,
+  ],
 })
 export class AppModule {}
