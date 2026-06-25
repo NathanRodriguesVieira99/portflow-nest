6a5eacfd0397a2e47cbbb7b0564ae32c8f551bda
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-25 16:29:58 -0300

    fix(prisma): add correct schema path

--- services/container-service/prisma.config.ts
+++ services/container-service/prisma.config.ts
@@ -2,7 +2,7 @@ import { env } from './src/config/env';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'prisma',
   migrations: {
     path: 'prisma/migrations',
     seed: 'tsx ./prisma/seed.ts',
