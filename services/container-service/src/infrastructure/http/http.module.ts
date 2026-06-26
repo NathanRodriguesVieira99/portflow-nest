047adb327aa9699ac7a94055ad2e811b6315e2fe
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-25 23:07:23 -0300

    feat(http): made HttpModule global

--- services/container-service/src/infrastructure/http/http.module.ts
+++ services/container-service/src/infrastructure/http/http.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { HttpClient } from './http-client';
 
+@Global()
 @Module({
   providers: [HttpClient],
   exports: [HttpClient],
