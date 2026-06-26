123f3ef42073e74aad95fd093e9ea300e9b26804
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-26 16:55:38 -0300

    style:  reorganize providers on ContainerModule

--- services/container-service/src/modules/container/container.module.ts
+++ services/container-service/src/modules/container/container.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
-import { ContainerService } from './application/services/container/container.service';
-import { ContainerController } from './presentation/controllers/container/container.controller';
+
+import { ContainerController } from './presentation/controllers/container.controller';
+import { HealthcheckController } from './presentation/controllers/healthcheck.controller';
+
+import { ContainerService } from './application/services/container.service';
+import { TerminalService } from './application/services/terminal.service';
+
+import { TerminalHttp } from './infra/http/terminal.http';
+
+import { ContainerProducer } from './presentation/events/container.producer';
+import { ContainerConsumer } from './presentation/events/container.consumer';
+
+import {
+  ContainerRepository,
+  IContainerRepository,
+} from './infra/repositories/container.repository';
 
 @Module({
-  providers: [ContainerService],
-  controllers: [ContainerController],
+  providers: [
+    /* Services */
+    ContainerService,
+    TerminalService,
+
+    /* HTTP */
+    TerminalHttp,
+
+    /* Kafka */
+    ContainerProducer,
+    ContainerConsumer,
+
+    /* Repositories */
+    { provide: IContainerRepository, useClass: ContainerRepository },
+  ],
+  controllers: [ContainerController, HealthcheckController],
 })
 export class ContainerModule {}
