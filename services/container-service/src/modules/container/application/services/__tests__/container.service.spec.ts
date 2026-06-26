758147f3544e4b0fb5fec52a26427b9e3c235231
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-26 16:56:42 -0300

    test(container): add providers with mocks

--- services/container-service/src/modules/container/application/services/__tests__/container.service.spec.ts
+++ services/container-service/src/modules/container/application/services/__tests__/container.service.spec.ts
@@ -1,4 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { TerminalService } from '../terminal.service';
+import { ContainerProducer } from '../../../presentation/events/container.producer';
+import { IContainerRepository } from '../../../infra/repositories/container.repository';
+
 import { ContainerService } from '../container.service';
 
 describe('ContainerService', () => {
@@ -6,7 +11,27 @@ describe('ContainerService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContainerService],
+      providers: [
+        ContainerService,
+        {
+          provide: IContainerRepository,
+          useValue: {
+            registerContainerArrival: vi.fn(),
+            findAllContainers: vi.fn(),
+            findContainerById: vi.fn(),
+            findStatusById: vi.fn(),
+            updateContainerStatus: vi.fn(),
+          },
+        },
+        {
+          provide: TerminalService,
+          useValue: { validateTerminal: vi.fn() },
+        },
+        {
+          provide: ContainerProducer,
+          useValue: { sendPendingDocumentationEvent: vi.fn() },
+        },
+      ],
     }).compile();
 
     service = module.get<ContainerService>(ContainerService);
