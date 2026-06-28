dab7445f96daba1117c77dd9a4763f31d8e42619
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-27 21:17:57 -0300

    feat(kafka): add dynamic auto topic creation

---
+++ services/container-service/src/infrastructure/kafka/kafka.config.ts
@@ -0,0 +1,20 @@
+import { Transport, type KafkaOptions } from '@nestjs/microservices';
+
+import { KAFKA_CLIENTS } from './constants/clients';
+import { KAFKA_CONSUMER_GROUPS } from './constants/groups';
+
+import { env } from '../../config/env';
+
+export const kafkaConfig: KafkaOptions = {
+  transport: Transport.KAFKA,
+  options: {
+    client: {
+      clientId: KAFKA_CLIENTS.CONTAINER_SERVICE,
+      brokers: [env.KAFKA_BROKER],
+    },
+    consumer: {
+      groupId: KAFKA_CONSUMER_GROUPS.CONTAINER_SERVICE,
+      allowAutoTopicCreation: env.NODE_ENV === 'development' ? true : false,
+    },
+  },
+};
