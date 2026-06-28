2caabd3331606d4e19e5c31030c822d00f2fedd2
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-27 23:00:10 -0300

    feat(container): add container service

--- services/container-service/src/modules/container/application/services/container.service.ts
+++ services/container-service/src/modules/container/application/services/container.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+import { TerminalService } from './terminal.service';
+
+import { ContainerProducer } from '../../presentation/events/container.producer';
+
+import { ContainerRepositoryContract } from '../../domain/repositories/container.repository.contract';
+
 @Injectable()
-export class ContainerService {}
+export class ContainerService {
+  constructor(
+    private readonly repository: ContainerRepositoryContract,
+    private readonly terminal: TerminalService,
+    private readonly kafka: ContainerProducer,
+  ) {}
+
+  async registerContainerArrival() {}
+}
