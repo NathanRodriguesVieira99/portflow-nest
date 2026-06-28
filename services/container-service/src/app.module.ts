69de4b19c140e8e5560850db9e1c75a706883691
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-27 21:07:52 -0300

    fix: add correct impot path to kafka module

--- services/container-service/src/app.module.ts
+++ services/container-service/src/app.module.ts
@@ -4,7 +4,7 @@ import { CacheModule } from './infrastructure/cache/cache.module';
 import { PrismaModule } from './infrastructure/database/prisma/prisma.module';
 import { LogsModule } from './infrastructure/observability/logs/logs.module';
 import { HttpModule } from './infrastructure/http/http.module';
-import { KafkaModule } from './infrastructure/messaging/kafka/kafka.module';
+import { KafkaModule } from './infrastructure/kafka/kafka.module';
 import { PrometheusModule } from './infrastructure/observability/prometheus/prometheus.module';
 import { ClsModule } from './infrastructure/observability/cls/cls.module';
 import { ContainerModule } from './modules/container/container.module';
