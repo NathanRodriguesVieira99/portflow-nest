5da45b2e8475705ae9213765ec337617ec05bf7a
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-07-02 20:29:14 -0300

    fix(k6): add correct environment variables

--- services/container-service/k6/scripts/load.ts
+++ services/container-service/k6/scripts/load.ts
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-const KONG_BASE_URL = `http://kong:8000/container/api/v1`;
+const KONG_BASE_URL = `http://${__ENV.NODE_ENV === 'development' ? 'localhost' : 'kong'}:8000/container/api/v1`;
 
 export default function () {
   const result = http.get(`${KONG_BASE_URL}/health`);
