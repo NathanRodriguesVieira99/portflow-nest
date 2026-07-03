4ce1890f5fdf928e7d7740030c644c908a6b022c
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-07-02 22:18:18 -0300

    refactor(k6): add docker host to base url

--- services/container-service/k6/scenarios/health.ts
+++ services/container-service/k6/scenarios/health.ts
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-const BASE_URL = `http://localhost:3333/api/v1`;
+const BASE_URL = 'http://container-service:3333/api/v1';
 
 export default function healthcheck() {
   const result = http.get(`${BASE_URL}/health`);
