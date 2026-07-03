c593689dbbb607c79e4f08d43b27989461076ca8
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-07-02 22:50:08 -0300

    feat(http): implement Result Pattern on http client
    Also add default headers to correlationId, remove unused create() static method

--- services/container-service/src/infrastructure/http/http-client.ts
+++ services/container-service/src/infrastructure/http/http-client.ts
@@ -1,8 +1,10 @@
 import { Injectable, Logger, OnModuleInit, Optional } from '@nestjs/common';
-
 import axios, { type AxiosError, type AxiosInstance } from 'axios';
-
 import { IHttpClient, HttpRequest } from './http-client.types';
+import { err, ok, type Result } from '../../shared/errors/result';
+import { HTTP_ERROR_CODES } from '../../shared/errors/http-codes';
+import { internalServerError } from '../../shared/errors/exceptions/exceptions';
+import { ClsService } from 'nestjs-cls';
 
 @Injectable()
 export class HttpClient implements IHttpClient, OnModuleInit {
@@ -12,11 +14,10 @@ export class HttpClient implements IHttpClient, OnModuleInit {
    @Optional diz ao NestJs para não lançar erro se não encontrar um provider para aquele parâmetro na DI.
    Em vez disso, ele deixa o parâmetro undefined ou com o valor default.
    */
-  constructor(@Optional() private readonly api: AxiosInstance = axios) {}
-
-  static create() {
-    return new HttpClient();
-  }
+  constructor(
+    @Optional() private readonly api: AxiosInstance = axios,
+    private readonly cls: ClsService,
+  ) {}
 
   onModuleInit() {
     this.logger.log('Axios HTTP Client Started!');
@@ -29,20 +30,23 @@ export class HttpClient implements IHttpClient, OnModuleInit {
     headers,
     params,
     body,
-  }: HttpRequest<T>) {
+  }: HttpRequest<T>): Promise<Result<R>> {
     try {
       const { data } = await this.api.request<R>({
         baseURL,
         url: endpoint,
         method,
-        headers,
+        headers: {
+          ...headers,
+          'x-correlation-id': this.cls.getId() ?? '', // toda request HTTP recebe o correlationId automaticamente via headers
+        },
         data: body,
         params,
       });
 
-      return data;
-    } catch (err) {
-      const error = err as AxiosError;
+      return ok(data);
+    } catch (e) {
+      const error = e as AxiosError;
 
       this.logger.error('HTTP ERROR', {
         message: error.message,
@@ -52,10 +56,13 @@ export class HttpClient implements IHttpClient, OnModuleInit {
         method: error.config?.method,
       });
 
-      const status = error.response?.status || 500;
+      const status =
+        error.response?.status || HTTP_ERROR_CODES.INTERNAL_SERVER_ERROR;
       const message = error.response?.data || error.message;
 
-      throw new Error(`Request failed with status ${status}: ${message}`);
+      return err(
+        internalServerError(`Request failed with status ${status}: ${message}`),
+      );
     }
   }
 }
