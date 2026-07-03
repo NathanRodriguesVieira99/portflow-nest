b1ef49d12933efe4e2ccf04a042bc057774c952e
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-07-02 21:51:57 -0300

    fix(k6): remove kong url to use isolated microservice url

--- services/container-service/k6/scenarios/health.ts
+++ services/container-service/k6/scenarios/health.ts
@@ -1,11 +1,10 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-const KONG_BASE_URL =
-  __ENV.KONG_BASE_URL || `http://kong:8000/container/api/v1`;
+const BASE_URL = `http://localhost:3333/container/api/v1`;
 
 export default function healthcheck() {
-  const result = http.get(`${KONG_BASE_URL}/health`);
+  const result = http.get(`${BASE_URL}/health`);
 
   console.log(result.status);
   console.log(result.body);
