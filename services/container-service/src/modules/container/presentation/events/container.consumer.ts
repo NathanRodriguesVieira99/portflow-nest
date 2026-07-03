73c16272d2b41a352b3c8ad6c3fc204010257a66
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-07-02 23:01:45 -0300

    feat(kafka,container): receive kafka metadata from context and set correlationId from parent trace

--- services/container-service/src/modules/container/presentation/events/container.consumer.ts
+++ services/container-service/src/modules/container/presentation/events/container.consumer.ts
@@ -1,22 +1,58 @@
 import { Controller, Logger } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import { Ctx, EventPattern, KafkaContext } from '@nestjs/microservices';
+import { ClsService } from 'nestjs-cls';
+import { context, propagation } from '@opentelemetry/api';
 import { KAFKA_TOPICS } from '../../../../infrastructure/kafka/constants/topics';
 
-import type { ContainerStatusEvent } from './contracts/container.events';
+import type { ContainerStatusEvent } from '../../domain/events/container.events';
 
 @Controller()
 export class ContainerConsumer {
   private logger = new Logger(ContainerConsumer.name);
 
+  constructor(private readonly cls: ClsService) {}
+
   @EventPattern(KAFKA_TOPICS.DOCUMENTATION_RELEASED)
-  receiveDocumentationReleased(event: ContainerStatusEvent) {
-    this.logger.log('Event Received!');
-    this.logger.log(event);
+  receiveDocumentationReleased(
+    event: ContainerStatusEvent,
+    @Ctx() kafkaContext: KafkaContext, // Dá acesso aos metadados da mensagem do kafka (aqui queremos os headers)
+  ) {
+    const headersComingFromKafka = kafkaContext.getMessage().headers ?? {};
+
+    const parentTraceContext = propagation.extract(
+      context.active(),
+      headersComingFromKafka,
+    );
+
+    context.with(parentTraceContext, () => {
+      this.cls.set(
+        'x-correlation-id',
+        headersComingFromKafka['x-correlation-id'] ?? event.correlationId,
+      );
+      this.logger.log('Documentation Received!');
+      this.logger.log(event);
+    });
   }
 
   @EventPattern(KAFKA_TOPICS.DOCUMENTATION_REFUSED)
-  receiveDocumentationRefused(event: ContainerStatusEvent) {
-    this.logger.log('Event Refused!');
-    this.logger.log(event);
+  receiveDocumentationRefused(
+    @Ctx() kafkaContext: KafkaContext,
+    event: ContainerStatusEvent,
+  ) {
+    const headersComingFromKafka = kafkaContext.getMessage().headers ?? {};
+
+    const parentTraceContext = propagation.extract(
+      context.active(),
+      headersComingFromKafka,
+    );
+
+    context.with(parentTraceContext, () => {
+      this.cls.set(
+        'x-correlation-id',
+        headersComingFromKafka['x-correlation-id'] ?? event.correlationId,
+      );
+      this.logger.log('Documentation Refused!');
+      this.logger.log(event);
+    });
   }
 }
