d2d0b6d74ebe489ee1e13a0eb44eb6787ec307e1
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-07-02 22:55:50 -0300

    fix(container,events): add correct status type, replace string value

--- services/container-service/src/modules/container/domain/events/container.events.ts
+++ services/container-service/src/modules/container/domain/events/container.events.ts
@@ -1,8 +1,10 @@
+import type { StatusContainer } from '../../@types/status-container.type';
+
 export interface ContainerStatusEvent {
   eventId: string;
   containerId: string;
-  previousStatus: string;
-  currentStatus: string;
+  previousStatus: StatusContainer;
+  currentStatus: StatusContainer;
   description: string;
   origin: string;
   dateTime: Date;
