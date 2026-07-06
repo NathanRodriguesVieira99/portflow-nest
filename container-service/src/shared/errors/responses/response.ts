27bb0951a531e018ea220cffbf86468d6e9dddfa
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-07-06 11:01:46 -0300

    refactor: update response type names to use on controllers

--- container-service/src/shared/errors/responses/response.ts
+++ container-service/src/shared/errors/responses/response.ts
@@ -1,4 +1,4 @@
 import type { ErrorResponse } from './error-response';
 import type { SuccessResponse } from './success-response';
 
-export type Response<T> = SuccessResponse<T> | ErrorResponse;
+export type RequestResponse<T> = SuccessResponse<T> | ErrorResponse;
