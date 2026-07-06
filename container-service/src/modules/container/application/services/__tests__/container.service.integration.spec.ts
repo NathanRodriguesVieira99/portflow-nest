631e8deff48c297f2d5eabd922f25c8e8b3fabea
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-07-06 20:54:34 -0300

    test(container): transform container service test file from unit  to integration

--- container-service/src/modules/container/application/services/__tests__/container.service.integration.spec.ts
+++ container-service/src/modules/container/application/services/__tests__/container.service.integration.spec.ts
@@ -1,44 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
 import { TerminalService } from '../terminal.service';
 import { ContainerService } from '../container.service';
-
-import { ContainerProducer } from '../../../infrastructure/events/container.producer';
-
 import { ContainerRepositoryContract } from '../../../domain/repositories/container.repository.contract';
+import { ContainerRepositoryImplementation } from '../../../infrastructure/repositories/container.repository.implementation';
+import { KafkaModule } from '../../../../../infrastructure/kafka/kafka.module';
+import { ClsModule } from '../../../../../infrastructure/observability/cls/cls.module';
+import { ContainerProducer } from '../../../infrastructure/events/container.producer';
+import { PrismaService } from '../../../../../infrastructure/database/prisma/prisma.service';
 
 describe('ContainerService', () => {
   let service: ContainerService;
 
-  const containerRepositoryMock = {
-    registerContainerArrival: vi.fn(),
-    findAllContainers: vi.fn(),
-    findContainerById: vi.fn(),
-    findStatusById: vi.fn(),
-    updateContainerStatus: vi.fn(),
-  };
-
   const terminalServiceMock = { validateTerminal: vi.fn() };
-
-  const sendPendingDocumentationEvent = {
-    sendPendingDocumentationEvent: vi.fn(),
-  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [KafkaModule, ClsModule],
       providers: [
+        PrismaService,
         ContainerService,
+        ContainerRepositoryImplementation,
+        ContainerProducer,
         {
           provide: ContainerRepositoryContract,
-          useValue: containerRepositoryMock,
+          useExisting: ContainerRepositoryImplementation,
         },
         {
           provide: TerminalService,
           useValue: terminalServiceMock,
-        },
-        {
-          provide: ContainerProducer,
-          useValue: sendPendingDocumentationEvent,
         },
       ],
     }).compile();
