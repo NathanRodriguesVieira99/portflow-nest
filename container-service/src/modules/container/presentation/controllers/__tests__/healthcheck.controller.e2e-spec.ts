1be8d53df35137f4e3495470fe9dd2114db1e6a5
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-07-08 19:31:19 -0300

    test(health): remove NestJs TestingModule setup

--- container-service/src/modules/container/presentation/controllers/__tests__/healthcheck.controller.e2e-spec.ts
+++ container-service/src/modules/container/presentation/controllers/__tests__/healthcheck.controller.e2e-spec.ts
@@ -1,25 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
-import { AppModule } from '../../../../../app.module';
-
-import type { INestApplication } from '@nestjs/common';
+import { app } from '../../../../../__tests__/helpers/setup-app';
 
 describe('E2E - Healthcheck', () => {
-  let app: INestApplication;
-
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  afterAll(async () => {
-    await app.close();
-  });
-
   describe('[E2E] GET /health', () => {
     it('should return 200', async () => {
       const health = await request(app.getHttpServer()).get(`/health`);
