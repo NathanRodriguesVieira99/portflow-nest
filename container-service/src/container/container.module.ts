749036e7615ab6b6b3996dd31c62339a5b374f6c
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-07-21 20:09:51 -0300

    refactor: instance http client and circuit breaker with useFactory

--- container-service/src/container/container.module.ts
+++ container-service/src/container/container.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
-
 import { ContainerController } from '@/container/presentation/controllers/container/container.controller';
 import { HealthcheckController } from '@/container/presentation/controllers/healthcheck/healthcheck.controller';
-
 import { ContainerService } from '@/container/application/services/container/container.service';
 import { TerminalService } from '@/container/application/services/terminal/terminal.service';
-
-import { TerminalHttp } from '@/container/infrastructure/http/terminal/terminal.http';
-
+import { HttpClient } from '@Infra/http/clients/http-client';
+import { TerminalHttp } from '@Infra/http/terminal/terminal.http';
+import { CircuitBreaker } from '@Infra/resilience/circuit-breaker';
 import { ContainerRepositoryContract } from '@Infra/persistence/repositories/prisma/container.repository.contract';
 import { ContainerRepositoryImplementation } from '@Infra/persistence/repositories/prisma/container.repository.implementation';
-
 import { SendPendingDocumentationEvent } from '@Infra/messaging/events/producers/send-pending-documentation.event';
 import { ReceiveDocumentationRefusedEvent } from '@Infra/messaging/events/consumers/receive-documentation-refused.event';
 import { ReceiveDocumentationReleasedEvent } from '@Infra/messaging/events/consumers/receive-documentation-released.event';
@@ -21,6 +18,14 @@ import { ReceiveDocumentationReleasedEvent } from '@Infra/messaging/events/consu
     TerminalService,
     TerminalHttp,
     SendPendingDocumentationEvent,
+    {
+      provide: HttpClient,
+      useFactory: () => HttpClient.create(),
+    },
+    {
+      provide: CircuitBreaker,
+      useFactory: () => CircuitBreaker.create(),
+    },
     {
       provide: ContainerRepositoryContract,
       useClass: ContainerRepositoryImplementation,
