bac5035c653d1838c73d0dbcc68e721250af0b46
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-07-21 18:40:17 -0300

    fix: update kafka config import path

--- container-service/src/main.ts
+++ container-service/src/main.ts
@@ -3,7 +3,7 @@ import './tracing';
 import { NestFactory } from '@nestjs/core';
 import { Logger as PinoLogger } from 'nestjs-pino';
 import { AppModule } from './app.module';
-import { kafkaConfig } from './infrastructure/messaging/kafka/kafka.config';
+import { kafkaConfig } from '@Infra/messaging/kafka/kafka.config';
 import { DocfyUiModule } from 'nestjs-docfy';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { env } from './shared/env';
