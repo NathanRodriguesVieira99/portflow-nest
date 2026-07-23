1ff3a16cbe4a168c55b1204bd0d2990157573331
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-07-23 20:04:57 -0300

    feat(persistence): add update method to container repository

--- container-service/src/container/infrastructure/persistence/repositories/prisma/container.repository.contract.ts
+++ container-service/src/container/infrastructure/persistence/repositories/prisma/container.repository.contract.ts
@@ -7,11 +7,9 @@ import type { PaginationOutput } from '@/container/application/contracts/paginat
 
 export abstract class ContainerRepositoryContract {
   abstract save(container: Container): Promise<Result<Container>>;
-
+  abstract update(container: Container): Promise<Result<Container>>;
   abstract remove(containerId: string): Promise<Result<string>>;
-
   abstract findById: (containerId: string) => Promise<Result<Container>>;
-
   abstract findAll: ({
     page,
     perPage,
@@ -20,5 +18,5 @@ export abstract class ContainerRepositoryContract {
   abstract findByStatus: (
     { page, perPage }: PaginationInput,
     status: StatusContainer,
-  ) => Promise<Result<PaginationOutput<Container | undefined>>>;
+  ) => Promise<Result<PaginationOutput<Container>>>;
 }
