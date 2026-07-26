070e037ce5e0c5fa4f7c27275107b8a18d1faa89
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-07-26 16:14:15 -0300

    refactor(resilience): extract CockatielAdapter and contracts into separate files

---
+++ container-service/src/container/infrastructure/resilience/adapters/cockatiel.adapter.ts
@@ -0,0 +1,62 @@
+import {
+  circuitBreaker,
+  ConsecutiveBreaker,
+  fallback,
+  handleAll,
+  wrap,
+  type CircuitBreakerPolicy,
+  CircuitState,
+} from 'cockatiel';
+
+import type {
+  CircuitBreakerState,
+  ResilienceContract,
+  ResilienceConfig,
+} from '../contracts/resilience.contracts';
+
+/**
+ * Adapter que implementa {@link ResilienceContract} utilizando a lib Cockatiel.
+ */
+export class CockatielAdapter implements ResilienceContract {
+  private readonly breakerPolicy: CircuitBreakerPolicy;
+
+  constructor(config: ResilienceConfig = {}) {
+    const { halfOpenAfter = 2000, consecutiveFailures = 5 } = config;
+
+    this.breakerPolicy = circuitBreaker(handleAll, {
+      halfOpenAfter,
+      breaker: new ConsecutiveBreaker(consecutiveFailures),
+    });
+  }
+
+  static create(config?: ResilienceConfig) {
+    return new CockatielAdapter(config);
+  }
+
+  getState(): CircuitBreakerState {
+    return this.mapCircuitState(this.breakerPolicy.state);
+  }
+
+  private mapCircuitState(state: CircuitState): CircuitBreakerState {
+    switch (state) {
+      case CircuitState.Closed:
+        return 'closed';
+      case CircuitState.Open:
+        return 'open';
+      case CircuitState.HalfOpen:
+        return 'half-open';
+      default:
+        throw new Error(`Unhandled circuit state: ${state}`);
+    }
+  }
+
+  async execute<T>(
+    fn: () => Promise<T>,
+    fallbackFn?: () => T | Promise<T>,
+  ): Promise<T> {
+    if (!fallbackFn) return this.breakerPolicy.execute(fn);
+    const fallbackPolicy = fallback<T>(handleAll, fallbackFn);
+    const policy = wrap(fallbackPolicy, this.breakerPolicy);
+    return policy.execute(fn);
+  }
+}
