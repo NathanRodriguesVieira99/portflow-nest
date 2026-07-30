bbbdfb567d7e1aaab16799783ac6fcab809e32db
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-07-30 20:22:28 -0300

    refactor(env): remove dynamicly .env file loading, improve validation and error handling

--- container-service/src/shared/env/index.ts
+++ container-service/src/shared/env/index.ts
@@ -1,23 +1,5 @@
-import { config } from 'dotenv';
-import { loadEnvFile } from 'node:process';
-import { Logger } from '@nestjs/common';
-import { z } from 'zod';
-
-/**
- * Carrega dinamicamente os arquivos .env ao rodar o projeto
- * - .env.local => roda o projeto fora do docker (necessita subir infra via docker)
- * - .env => roda o projeto dentro do docker + infra
- */
-const loadEnvFiles = () => {
-  try {
-    loadEnvFile('.env.local');
-  } catch {
-    loadEnvFile('.env');
-  }
-};
-
-loadEnvFiles();
-config();
+import 'dotenv/config';
+import * as z from 'zod';
 
 export const envSchema = z.object({
   SERVICE_NAME: z.string().default('container-service'),
@@ -38,14 +20,11 @@ export const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env);
 
-if (_env.success === false) {
-  if (process.env.NODE_ENV === 'development') {
-    Logger.error(
-      'Error on validate environment variables!',
-      z.treeifyError(_env.error),
-    );
-  }
-  throw new Error('Invalid environment variables!');
+if (!_env.success) {
+  const issues = _env.error.issues
+    .map((i) => `${i.path.join('.')}: ${i.message}`)
+    .join('\n');
+  throw new Error(`Invalid environment variables:\n${issues}`);
 }
 
 export const env = _env.data;
