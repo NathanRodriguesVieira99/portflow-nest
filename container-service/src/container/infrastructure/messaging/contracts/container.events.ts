02f7554d6c30003d711b95f8089463ad794195e1
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-01 19:38:54 -0300

    refactor(kafka): remove some generic fields, they will be auto generated

--- container-service/src/container/infrastructure/messaging/contracts/container.events.ts
+++ container-service/src/container/infrastructure/messaging/contracts/container.events.ts
@@ -1,12 +1,8 @@
 import type { StatusContainer } from '@Types/status-container.type';
 
 export interface ContainerStatusEvent {
-  eventId: string;
   containerId: string;
   previousStatus: StatusContainer;
   currentStatus: StatusContainer;
   description: string;
-  origin: string;
-  dateTime: Date;
-  correlationId: string;
 }
