5001e4d3e26d58907dcd36ff781c2b367b7f34cf
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-01 19:38:13 -0300

    refactor(kafka): isolate event builder method to a util method to create any type of events with default values

---
+++ container-service/src/container/infrastructure/messaging/events/build-event.ts
@@ -0,0 +1,28 @@
+import { nanoid } from 'nanoid';
+import { ClsService } from 'nestjs-cls';
+
+export type Event<P> = {
+  meta: {
+    eventId: string;
+    correlationId: string;
+    origin: string;
+    occurredAt: Date;
+  };
+  payload: P;
+};
+
+export class EventBuilder {
+  constructor(private readonly cls: ClsService) {}
+
+  build<P>(origin: string, payload: P): Event<P> {
+    return {
+      meta: {
+        eventId: nanoid(),
+        correlationId: this.cls.getId(),
+        origin,
+        occurredAt: new Date(),
+      },
+      payload,
+    };
+  }
+}
--- container-service/src/container/infrastructure/messaging/events/producers/send-pending-documentation.event.ts
+++ container-service/src/container/infrastructure/messaging/events/producers/send-pending-documentation.event.ts
@@ -1,44 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { ClsService } from 'nestjs-cls';
-import { nanoid } from 'nanoid';
-import { env } from '@Shared/env';
+import { EventBuilder } from '../build-event';
 import { KAFKA_TOPICS } from '@Shared/constants/kafka';
-import { KafkaProducer } from '@/container/infrastructure/messaging/kafka/kafka.producer';
+import { env } from '@Shared/env';
 
-import type { ContainerStatusEvent } from '@/container/infrastructure/messaging/contracts/container.events';
-import type { StatusContainer } from '@Types/status-container.type';
+import type { ContainerStatusEvent } from '../../contracts/container.events';
+import type { MessageBrokerProducerContract } from '../../contracts/message-broker.contract';
 
 @Injectable()
 export class SendPendingDocumentationEvent {
   constructor(
-    private readonly kafka: KafkaProducer,
-    private readonly cls: ClsService,
+    private readonly kafka: MessageBrokerProducerContract,
+    private readonly event: EventBuilder,
   ) {}
 
-  private buildEvent(
-    containerId: string,
-    previousStatus: StatusContainer,
-    currentStatus: StatusContainer,
-    description: string,
-  ): ContainerStatusEvent {
-    return {
-      eventId: nanoid(),
-      containerId,
-      previousStatus,
-      currentStatus,
-      description,
-      origin: env.SERVICE_NAME ?? 'container-service',
-      dateTime: new Date(),
-      correlationId: this.cls.getId(),
-    };
-  }
-
   async sendPendingDocumentation(containerId: string): Promise<void> {
-    const event = this.buildEvent(
-      containerId,
-      'ARRIVED',
-      'PENDING_DOCUMENTATION',
-      `The container ${containerId} is waiting the documentation`,
+    const event = this.event.build<ContainerStatusEvent>(
+      env.SERVICE_NAME ?? 'container-service',
+      {
+        containerId,
+        previousStatus: 'ARRIVED',
+        currentStatus: 'PENDING_DOCUMENTATION',
+        description: `The container ${containerId} is waiting the documentation`,
+      },
     );
 
     await this.kafka.produce(
