52cd3d17dfc7a4b90ec0855055ae12aad424ee41
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-03 19:28:42 -0300

    feat(terminal): add result, error and response shared utilities

---
+++ terminal-service/src/shared/responses/index.ts
@@ -0,0 +1,43 @@
+import {
+  HTTP_ERROR,
+  HTTP_SUCCESS,
+  type HttpError,
+  type HttpSuccess,
+} from '../constants/http-codes';
+
+export namespace Response {
+  export type Error = {
+    ok: false;
+    error: {
+      code: HttpError;
+      status: number;
+      message: string;
+    };
+    timestamp: string;
+  };
+  export type Success<T> = {
+    ok: true;
+    response: { code: HttpSuccess; status: number; content: T };
+    timestamp: string;
+  };
+}
+
+export const errorResponse = (
+  code: HttpError,
+  message: string,
+): Response.Error => ({
+  ok: false,
+  error: { code, status: HTTP_ERROR[code].status, message },
+  timestamp: new Date().toISOString(),
+});
+
+export const successResponse = <T = unknown>(
+  content: T,
+  code: HttpSuccess = 'OK',
+): Response.Success<T> => ({
+  ok: true,
+  response: { code, status: HTTP_SUCCESS[code].status, content },
+  timestamp: new Date().toISOString(),
+});
+
+export type RequestResponse<T> = Response.Success<T> | Response.Error;
