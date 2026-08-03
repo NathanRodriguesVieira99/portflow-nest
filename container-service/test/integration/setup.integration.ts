53b242f5d951a7a01265eb507809a62790d25484
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-03 15:38:16 -0300

    refactor: remove nestjs testing module from integration tests setup file

--- container-service/test/integration/setup.integration.ts
+++ container-service/test/integration/setup.integration.ts
@@ -1,25 +1,21 @@
 import {
-  closeTestingModule,
   disconnectKafka,
   disconnectPrismaAndPostgres,
   resetDatabase,
   setupKafka,
   setupPrismaAndPostgres,
-  setupTestingModule,
 } from '../helpers';
 
 beforeAll(async () => {
-  await setupTestingModule();
   await setupKafka();
   await setupPrismaAndPostgres();
 });
 
-beforeEach(() => {
-  resetDatabase();
+beforeEach(async () => {
+  await resetDatabase();
 });
 
 afterAll(async () => {
   await disconnectPrismaAndPostgres();
   await disconnectKafka();
-  await closeTestingModule();
 });
