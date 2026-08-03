ee612d0a10638482e7d2cb599ceea97acfea96b1
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-03 19:28:49 -0300

    feat(terminal): add healthcheck controller

---
+++ terminal-service/src/terminal/presentation/controllers/healthcheck/healthcheck.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+import { successResponse } from '@Shared/responses';
+
+@Controller('/health')
+export class HealthcheckController {
+  @Get()
+  health() {
+    return successResponse('Container service UP!', 'OK');
+  }
+}
