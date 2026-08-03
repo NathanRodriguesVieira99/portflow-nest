988d4871a759d2d4b0fb33c61c971a93a603a3f6
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-03 15:37:37 -0300

    refactor: remove logging from setup kafka testcontainers

--- container-service/test/helpers/setup-kafka.ts
+++ container-service/test/helpers/setup-kafka.ts
@@ -1,21 +1,15 @@
 import { PinoLogger } from 'nestjs-pino';
 import { KafkaContainer, StartedKafkaContainer } from '@testcontainers/kafka';
 
-const logger = new PinoLogger({ renameContext: 'Setup Kafka' });
-
 export let kafka: StartedKafkaContainer;
 
 export const setupKafka = async () => {
   kafka = await new KafkaContainer('confluentinc/cp-kafka:7.8.0')
     .withKraft()
     .start();
-
   process.env.KAFKA_BROKER = `${kafka.getHost()}:${kafka.getMappedPort(9093)}`;
-
-  logger.info('Kafka connected!');
 };
 
 export const disconnectKafka = async () => {
   await kafka.stop();
-  logger.info('Kafka disconnected!');
 };
