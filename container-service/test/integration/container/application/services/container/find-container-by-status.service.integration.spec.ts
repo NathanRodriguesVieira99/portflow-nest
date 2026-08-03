54971e1bcfb2fdbf759d2663200f67affd5a37bd
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-03 15:50:57 -0300

    test(container): remove NestJs from integration tests

--- container-service/test/integration/container/application/services/container/find-container-by-status.service.integration.spec.ts
+++ container-service/test/integration/container/application/services/container/find-container-by-status.service.integration.spec.ts
@@ -1,40 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@/container/infrastructure/persistence/database/prisma/prisma.service';
+import { ContainerRepositoryImplementation } from '@/container/infrastructure/persistence/repositories/prisma/container.repository.implementation';
 import { FindContainerByStatusService } from '@/container/application/services/container/find-container-by-status.service';
-import { ClsModule } from '@Infra/observability/cls.module';
-import { SendPendingDocumentationEvent } from '@Infra/messaging/events/producers/send-pending-documentation.event';
-import { TerminalHttp } from '@Infra/http/terminal/terminal.http';
-import { KafkaModule } from '@Infra/messaging/kafka.module';
-import { PrismaService } from '@Infra/persistence/database/prisma/prisma.service';
-import { AxiosAdapter } from '@/container/infrastructure/http/adapters/axios.adapter';
-import { ContainerRepositoryImplementation } from '@Infra/persistence/repositories/prisma/container.repository.implementation';
-import { CONTAINER_REPOSITORY_CONTRACT } from '@Infra/persistence/repositories/prisma/container.repository.contract';
+
+import type { ContainerRepositoryContract } from '@Infra/persistence/repositories/prisma/container.repository.contract';
 
 describe('ContainerService', () => {
+  let prisma: PrismaService;
   let service: FindContainerByStatusService;
+  let repo: ContainerRepositoryContract;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [KafkaModule, ClsModule],
-      providers: [
-        PrismaService,
-        FindContainerByStatusService,
-        SendPendingDocumentationEvent,
-        TerminalHttp,
-        AxiosAdapter,
-        ContainerRepositoryImplementation,
-        {
-          provide: CONTAINER_REPOSITORY_CONTRACT,
-          useExisting: ContainerRepositoryImplementation,
-        },
-      ],
-    }).compile();
-
-    service = module.get<FindContainerByStatusService>(
-      FindContainerByStatusService,
-    );
+    prisma = new PrismaService();
+    repo = new ContainerRepositoryImplementation(prisma);
+    service = new FindContainerByStatusService(repo);
   });
 
-  it.todo('should be defined', () => {
+  it('should be defined', () => {
     expect(service).toBeDefined();
   });
 });
--- container-service/test/integration/container/infrastructure/repositories/prisma/container.repository.implementation.integration.spec.ts
+++ container-service/test/integration/container/infrastructure/repositories/prisma/container.repository.implementation.integration.spec.ts
@@ -1,3 +1,20 @@
+import { PrismaService } from '@/container/infrastructure/persistence/database/prisma/prisma.service';
+import { ContainerRepositoryImplementation } from '@/container/infrastructure/persistence/repositories/prisma/container.repository.implementation';
+
+import type { ContainerRepositoryContract } from '@/container/infrastructure/persistence/repositories/prisma/container.repository.contract';
+
 describe('Container Repository Implementation', () => {
+  let prisma: PrismaService;
+  let repo: ContainerRepositoryContract;
+
+  beforeEach(async () => {
+    prisma = new PrismaService();
+    repo = new ContainerRepositoryImplementation(prisma);
+  });
+
+  it('should be defined', () => {
+    expect(repo).toBeDefined();
+  });
+
   it.todo('should', () => {});
 });
