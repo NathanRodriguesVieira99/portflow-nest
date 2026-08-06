a33fe861afca6f397b97f5baebf3637001eeab2d
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-06 16:27:58 -0300

    fix(terminal): inject schemas on mongoose module

--- microservices/terminal-service/src/terminal/infrastructure/persistence/database/mongodb/mongoose.module.ts
+++ microservices/terminal-service/src/terminal/infrastructure/persistence/database/mongodb/mongoose.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule as Mongoose } from '@nestjs/mongoose';
+import {
+  TerminalSchema,
+  TerminalSchemaDefinition,
+} from './schemas/terminal.schema';
 import { env } from '@Shared/env';
 
 @Module({
-  imports: [Mongoose.forRoot(env.MONGODB_URI, {})],
+  imports: [
+    Mongoose.forRoot(env.MONGODB_URI, {}),
+    Mongoose.forFeature([
+      {
+        name: TerminalSchema.name,
+        schema: TerminalSchemaDefinition,
+      },
+    ]),
+  ],
 })
 export class MongooseModule {}
