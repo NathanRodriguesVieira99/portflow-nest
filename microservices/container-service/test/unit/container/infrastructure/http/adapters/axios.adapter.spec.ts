da6b653e72fd5d6174629297beb55f9f262db10c
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-07 13:03:26 -0300

    test(container): improve axios adapter tests by using nock + sinon

--- microservices/container-service/test/unit/container/infrastructure/http/adapters/axios.adapter.spec.ts
+++ microservices/container-service/test/unit/container/infrastructure/http/adapters/axios.adapter.spec.ts
@@ -1,178 +1,205 @@
+import nock from 'nock';
+import Sinon from 'sinon';
 import axios, { AxiosError } from 'axios';
-import { AxiosAdapter } from '@Infra/http/';
-import { internalServerError } from '@/container/application/exceptions';
+import { AxiosAdapter, type HttpClientContract } from '@Infra/http/';
+
+import type { HttpCodes } from '@/shared/constants/http-codes';
 
 describe('Http Client', () => {
-  const http = AxiosAdapter.create();
+  let sut: HttpClientContract;
+
+  beforeAll(() => {
+    sut = AxiosAdapter.create();
+  });
 
   describe('request', () => {
     describe('Http Methods', () => {
       it('should make a successful GET request and return data', async () => {
-        const mockResponse = { data: 'Test Data' };
-
-        vi.spyOn(axios, 'request').mockResolvedValueOnce({
-          data: mockResponse,
-        });
-
-        const result = await http.request<typeof mockResponse, never>({
-          url: 'http://fake-endpoint/test',
+        const url = 'http://localhost:4321';
+        const endpoint = '/test';
+        const expectedHttpCode: HttpCodes = 200;
+        const expectedResponseBody = { data: 'Test Data' };
+        nock(url).get(endpoint).reply(expectedHttpCode, expectedResponseBody);
+        const getSpy = Sinon.spy(axios, 'request');
+        const response = await sut.request<typeof expectedResponseBody, never>({
+          url: `${url}${endpoint}`,
           method: 'GET',
           headers: { 'Content-Type': 'application/json' },
+          body: undefined,
           params: undefined,
         });
-
-        expect(result).toEqual({
+        expect(getSpy.calledOnce).toBeTruthy();
+        expect(response).toEqual({
           ok: true,
           value: {
-            data: mockResponse.data,
+            data: expectedResponseBody,
+            status: expectedHttpCode,
           },
         });
-        expect(axios.request).toHaveBeenCalledWith({
-          url: 'http://fake-endpoint/test',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-correlation-id': '',
-          },
-          data: undefined,
-          method: 'GET',
-          params: undefined,
-        });
+        expect(
+          getSpy.calledWith({
+            url: `${url}${endpoint}`,
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'x-correlation-id': '',
+            },
+            data: undefined,
+            params: undefined,
+          }),
+        ).toBeTruthy();
       });
-
       it('should make a successful GET request with query params', async () => {
-        const mockResponse = { data: 'Test Data' };
-
-        vi.spyOn(axios, 'request').mockResolvedValueOnce({
-          data: mockResponse,
-        });
-
-        const result = await http.request<typeof mockResponse, never>({
-          url: 'http://fake-endpoint/test',
+        const url = 'http://localhost:4321';
+        const endpoint = '/test';
+        const expectedHttpCode: HttpCodes = 200;
+        const expectedResponseBody = { data: 'Test Data' };
+        nock(url)
+          .get(endpoint)
+          .query({ page: 1, perPage: 10 })
+          .reply(expectedHttpCode, expectedResponseBody);
+        const getSpy = Sinon.spy(axios, 'request');
+        const response = await sut.request<typeof expectedResponseBody, never>({
+          url: `${url}${endpoint}`,
           method: 'GET',
           headers: { 'Content-Type': 'application/json' },
           params: { page: 1, perPage: 10 },
         });
-
-        expect(result).toEqual({
+        expect(response).toEqual({
           ok: true,
           value: {
-            data: mockResponse.data,
+            data: expectedResponseBody,
+            status: expectedHttpCode,
           },
         });
-        expect(axios.request).toHaveBeenCalledWith({
-          url: 'http://fake-endpoint/test',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-correlation-id': '',
-          },
-          data: undefined,
-          method: 'GET',
-          params: { page: 1, perPage: 10 },
-        });
+        expect(getSpy.calledOnce).toBeTruthy();
+        expect(
+          getSpy.calledWith({
+            url: `${url}${endpoint}`,
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'x-correlation-id': '',
+            },
+            data: undefined,
+            params: { page: 1, perPage: 10 },
+          }),
+        ).toBeTruthy();
       });
-
       it('should make a successful GET request with custom headers', async () => {
-        const mockResponse = { data: 'Test Data' };
-
-        vi.spyOn(axios, 'request').mockResolvedValueOnce({
-          data: mockResponse,
-        });
-
-        const result = await http.request<typeof mockResponse, never>({
-          url: 'http://fake-endpoint/test',
+        const url = 'http://localhost:4321';
+        const endpoint = '/test';
+        const expectedHttpCode: HttpCodes = 200;
+        const expectedResponseBody = { data: 'Test Data' };
+        nock(url).get(endpoint).reply(expectedHttpCode, expectedResponseBody);
+        const getSpy = Sinon.spy(axios, 'request');
+        const response = await sut.request<typeof expectedResponseBody, never>({
+          url: `${url}${endpoint}`,
           method: 'GET',
           headers: { Authorization: 'Bearer token' },
           params: undefined,
         });
-
-        expect(result).toEqual({
+        expect(getSpy.calledOnce).toBeTruthy();
+        expect(response).toEqual({
           ok: true,
           value: {
-            data: mockResponse.data,
+            data: expectedResponseBody,
+            status: expectedHttpCode,
           },
         });
-        expect(axios.request).toHaveBeenCalledWith({
-          url: 'http://fake-endpoint/test',
-          headers: { Authorization: 'Bearer token', 'x-correlation-id': '' },
-          data: undefined,
-          method: 'GET',
-          params: undefined,
-        });
+        expect(
+          getSpy.calledWith({
+            url: `${url}${endpoint}`,
+            headers: { Authorization: 'Bearer token', 'x-correlation-id': '' },
+            data: undefined,
+            method: 'GET',
+            params: undefined,
+          }),
+        ).toBeTruthy();
       });
-
       it('should make a successful POST request and return data', async () => {
-        const mockResponse = { data: 'Test Data' };
-
-        vi.spyOn(axios, 'request').mockResolvedValueOnce({
-          data: mockResponse,
-        });
-
-        const result = await http.request<
-          typeof mockResponse,
-          { name: string }
+        const url = 'http://localhost:4321';
+        const endpoint = '/test';
+        const expectedHttpCode: HttpCodes = 201;
+        const requestBody = { data: 'Test Request Data' };
+        const expectedResponseBody = { data: 'Test Response Data' };
+        nock(url).post(endpoint).reply(expectedHttpCode, expectedResponseBody);
+        const postSpy = Sinon.spy(axios, 'request');
+        const response = await sut.request<
+          typeof expectedResponseBody,
+          typeof requestBody
         >({
-          url: 'http://fake-endpoint/test',
+          url: `${url}${endpoint}`,
           method: 'POST',
-          body: { name: 'Test' },
+          body: requestBody,
           headers: { 'Content-Type': 'application/json' },
           params: undefined,
         });
-
-        expect(result).toEqual({
+        expect(postSpy.calledOnce).toBeTruthy();
+        expect(
+          postSpy.calledWith({
+            url: `${url}${endpoint}`,
+            headers: {
+              'Content-Type': 'application/json',
+              'x-correlation-id': '',
+            },
+            data: requestBody,
+            method: 'POST',
+            params: undefined,
+          }),
+        ).toBeTruthy();
+        expect(response).toEqual({
           ok: true,
           value: {
-            data: mockResponse.data,
+            data: expectedResponseBody,
+            status: expectedHttpCode,
           },
-        });
-        expect(axios.request).toHaveBeenCalledWith({
-          url: 'http://fake-endpoint/test',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-correlation-id': '',
-          },
-          data: { name: 'Test' },
-          method: 'POST',
-          params: undefined,
         });
       });
-
       it('should make a successful POST request with empty body', async () => {
-        const mockResponse = { data: 'Test Data' };
-
-        vi.spyOn(axios, 'request').mockResolvedValueOnce({
-          data: mockResponse,
-        });
-
-        const result = await http.request<typeof mockResponse, null>({
-          url: 'http://fake-endpoint/test',
+        const url = 'http://localhost:4321';
+        const endpoint = '/test';
+        const expectedHttpCode: HttpCodes = 201;
+        const expectedResponseBody = { data: 'Test Response Data' };
+        nock(url).post(endpoint).reply(expectedHttpCode, expectedResponseBody);
+        const postSpy = Sinon.spy(axios, 'request');
+        const result = await sut.request<typeof expectedResponseBody, null>({
+          url: `${url}${endpoint}`,
           method: 'POST',
           body: null,
           headers: { 'Content-Type': 'application/json' },
           params: undefined,
         });
-
+        expect(postSpy.calledOnce).toBeTruthy();
         expect(result).toEqual({
           ok: true,
           value: {
-            data: mockResponse.data,
+            data: expectedResponseBody,
+            status: expectedHttpCode,
           },
         });
-        expect(axios.request).toHaveBeenCalledWith({
-          url: 'http://fake-endpoint/test',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-correlation-id': '',
-          },
-          data: null,
-          method: 'POST',
-          params: undefined,
-        });
+        expect(
+          postSpy.calledWith({
+            url: `${url}${endpoint}`,
+            headers: {
+              'Content-Type': 'application/json',
+              'x-correlation-id': '',
+            },
+            data: null,
+            method: 'POST',
+            params: undefined,
+          }),
+        ).toBeTruthy();
       });
+      it.todo('should make a successful DELETE request', async () => {});
+      it.todo('should make a successful PUT request', async () => {});
+      it.todo('should make a successful PATCH request', async () => {});
     });
-
     describe('Errors', () => {
-      it('should throw an axios error when request fails', async () => {
-        const axiosError = new AxiosError(
+      it.only('should throw an axios error when request fails', async () => {
+        const url = 'http://localhost:4321';
+        const endpoint = '/test';
+        const axiosRequestError = new AxiosError(
           'request failed',
           undefined,
           undefined,
@@ -185,67 +212,38 @@ describe('Http Client', () => {
             config: {} as any,
           },
         );
-
-        vi.spyOn(axios, 'request').mockRejectedValueOnce(axiosError);
-
-        const result = await http.request<null, null>({
-          url: 'http://fake-endpoint/test',
+        const axiosResponseError = {
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Request failed with status 500 : [object Object]',
+          status: 500,
+        };
+        const postStub = Sinon.stub(axios, 'request').rejects(
+          axiosRequestError,
+        );
+        const response = await sut.request<null, null>({
+          url: `${url}${endpoint}`,
           body: null,
           headers: { 'Content-Type': 'application/json' },
-          method: 'GET',
+          method: 'POST',
           params: undefined,
         });
-
-        expect(result).toEqual({
+        expect(postStub.calledOnce).toBeTruthy();
+        expect(response).toEqual({
           ok: false,
-          error: expect.objectContaining({
-            code: 'INTERNAL_SERVER_ERROR',
-            message: 'Request failed with status 500 : [object Object]',
-            status: 500,
+          error: axiosResponseError,
+        });
+        expect(
+          postStub.calledWith({
+            url: `${url}${endpoint}`,
+            headers: {
+              'Content-Type': 'application/json',
+              'x-correlation-id': '',
+            },
+            data: null,
+            method: 'POST',
+            params: undefined,
           }),
-        });
-        expect(axios.request).toHaveBeenCalledWith({
-          url: 'http://fake-endpoint/test',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-correlation-id': '',
-          },
-          data: null,
-          method: 'GET',
-          params: undefined,
-        });
-      });
-
-      it('should throw an generic error when request fails', async () => {
-        vi.spyOn(axios, 'request').mockRejectedValueOnce(internalServerError());
-
-        const result = await http.request<null, null>({
-          url: 'http://fake-endpoint/test',
-          body: null,
-          headers: { 'Content-Type': 'application/json' },
-          method: 'GET',
-          params: undefined,
-        });
-
-        expect(result).toEqual({
-          ok: false,
-          error: expect.objectContaining({
-            code: 'INTERNAL_SERVER_ERROR',
-            message:
-              'Request failed with status [object Object] : Internal server error!',
-            status: 500,
-          }),
-        });
-        expect(axios.request).toHaveBeenCalledWith({
-          url: 'http://fake-endpoint/test',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-correlation-id': '',
-          },
-          data: null,
-          method: 'GET',
-          params: undefined,
-        });
+        ).toBeTruthy();
       });
     });
   });
