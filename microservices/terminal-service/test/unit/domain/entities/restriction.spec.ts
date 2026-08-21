1c58c5488011fe168b09ba684fe11e981156eac7
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-21 18:12:59 -0300

    test(terminal): add unit test for Restriction entity

--- microservices/terminal-service/test/unit/domain/entities/restriction.spec.ts
+++ microservices/terminal-service/test/unit/domain/entities/restriction.spec.ts
@@ -1,5 +1,22 @@
+import { Restriction } from '@/domain/entities/restriction';
+
 describe('Restriction', () => {
   describe('create()', () => {
-    it('', () => {});
+    it('should create a Restriction entity', () => {
+      const acceptsDangerousCargo = true;
+      const acceptsRefrigeratedCargo = true;
+      const maxHeightInMeters = 10;
+      const maxWeightInTons = 5;
+      const restriction = Restriction.create({
+        acceptsDangerousCargo,
+        acceptsRefrigeratedCargo,
+        maxHeightInMeters,
+        maxWeightInTons,
+      });
+      expect(restriction.getAcceptsDangerousCargo()).toBeTruthy();
+      expect(restriction.getAcceptsRefrigeratedCargo()).toBeTruthy();
+      expect(restriction.getMaxHeightInMeters()).toEqual(10);
+      expect(restriction.getMaxWeightInTons()).toEqual(5);
+    });
   });
 });
